939c0bc8d3515f836674308bd778f43e60b5ce62
Martin Aegidius <gygridd@gmail.com>
2022-12-01 10:36:36 +0100

    push before implementing save model

--- timm_scripts/vit_callscript_larger_layers.py
+++ timm_scripts/vit_callscript_larger_layers.py
@@ -5,16 +5,15 @@ import math
 #exp for checking overfitting on full set
 
 
-L = [375] #[300] #[30,60,120,200]#[2,4,8,16,32]
-EPOCHS = [58,27]
+L = [999] #[300] #[30,60,120,200]#[2,4,8,16,32]
+EPOCHS = [120,120,120]
 
-num_layers = [6,9]
+num_layers = [5,6,9]
 num_heads = 1
 EVAL = 1
 
 #length = L[classOC]
-for epoch in EPOCHS:
-    for NL in num_layers:
-    	command = "python3 timm_pipe_no_overfit.py " + str(999) + " "+ str(370) + " " + str(NL) + " " + str(num_heads) + " " + str(epoch) + " " + str(EVAL)
-    	#print(command)
-    	os.system(command)
+for i, epoch in enumerate(EPOCHS):
+    command = "python3 timm_pipe_no_overfit.py " + str(999) + " "+ str(999) + " " + str(num_layers[i]) + " " + str(num_heads) + " " + str(epoch) + " " + str(EVAL)
+    #print(command)
+    os.system(command)
